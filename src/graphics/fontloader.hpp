1790c82409bd1533f297a829ac45856a0b50f6c6
lcd8891 <lcd8891@gmail.com>
2025-12-07 23:12:45 +0300

    i forgot fontloader

--- src/graphics/fontloader.hpp
+++ src/graphics/fontloader.hpp
@@ -1,17 +1,10 @@
 #pragma once
 #include <string>
 
-namespace LiteAPI{
-    class Texture;
-}
-
 namespace PRIV{
 
     namespace FontLoader{
-        extern const LiteAPI::Texture *glyph_atlas_ptr;
-        extern const uint8 *character_size_ptr;
-        void loadfrom(std::string _path);
-        const GlyphMetaData& getGlyphMetaData(wchar_t character);
+        void init();
         void close();
     }
 }
